a8687a5b63e6fdcfeeaf5f59dc4c0c2b715a2627
shehryaarsharif <shehryaarsharif27@gmail.com>
2020-06-08 03:55:17 +0500

    Added Shop Now Route

--- src/components/landing/Introduction.js
+++ src/components/landing/Introduction.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import {Button} from 'react-bootstrap';
-import ReactDOM from 'react-dom';
 import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
 import { Carousel } from 'react-responsive-carousel';
 import './Introduction.css';
-import Page1 from './Page1.jpg';
-import aboutus from './aboutus.jpg'
+import aboutus from './aboutus.jpg';
+
+import { Link, withRouter } from "react-router-dom";
+
 class Introduction extends Component {
     render() {
         return (
@@ -15,22 +16,23 @@ class Introduction extends Component {
                     <img id="img-1" src={aboutus}  />
                     <div className="container">
                         <div className="centered"> 
-                        <Button variant="light" style={{padding:"15px"}}><h4>SHOP NOW</h4></Button>
+                        <Button variant="light" style={{padding:"15px"}}> <Link to="/shop" ><h4 style={{color:"black"}}>SHOP NOW</h4></Link></Button>
                         </div>
                         {/* <div className=" legend pointer dim"><p className="txt"> GOSHOPNOW </p></div> */}
                     </div>
                 </div>
                 <div>
                     <img id="img-2" src={aboutus} />
-                    <div className="centered"> 
-                        <Button variant="light" style={{padding:"15px"}}><h4>SHOP NOW</h4></Button>
+                    <div className="centered">
+                        <Button variant="light" style={{padding:"15px"}}> <Link to="/shop" ><h4 style={{color:"black"}}>SHOP NOW</h4></Link></Button>
+                        {/* <Button variant="light" style={{padding:"15px"}}><h4>SHOP NOW</h4></Button> */}
                         </div>
                     {/* <div className=" legend pointer dim"><p className="txt"> GOSHOPNOW </p></div> */}
                 </div>
                 <div>
                     <img id="img-3" src={aboutus} />
-                    <div className="centered"> 
-                        <Button variant="light" style={{padding:"15px"}}><h4>SHOP NOW</h4></Button>
+                    <div className="centered">
+                        <Button variant="light" style={{padding:"15px"}}> <Link to="/shop" ><h4 style={{color:"black"}}>SHOP NOW</h4></Link></Button>
                     </div>
                     {/* <div className=" legend pointer dim"><p className="txt"> GOSHOPNOW </p></div> */}
                 </div>
@@ -40,5 +42,5 @@ class Introduction extends Component {
     }
 };
 
-export default Introduction;
+export default withRouter(Introduction);
  
